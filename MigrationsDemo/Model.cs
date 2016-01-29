5fd8988ea01bc53b51da6fbb4ab425ce3b788632
Daniel Sundberg <daniel.sundberg88@gmail.com>
2016-01-29 22:01:34 +0100

    migrations demo

---
+++ MigrationsDemo/Model.cs
@@ -0,0 +1,16 @@
+﻿using System.Data.Entity;
+
+namespace MigrationsDemo
+{
+    public class BlogContext : DbContext
+    {
+        public DbSet<Blog> Blogs { get; set; }
+    }
+
+    public class Blog
+    {
+        public int BlogId { get; set; }
+        public string Name { get; set; }
+        public string Test { get; set; }
+    }
+}
---
+++ MigrationsDemo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace MigrationsDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Recreate database on change
+            Database.SetInitializer<BlogContext>(new DropCreateDatabaseIfModelChanges<BlogContext>());
+
+            using (var db = new BlogContext())
+            {
+                db.Blogs.Add(new Blog { Name = "Another Blog ", Test = "Hejhej"});
+                db.SaveChanges();
+
+                foreach (var blog in db.Blogs)
+                {
+                    Console.WriteLine(blog.Name);
+                }
+            }
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
